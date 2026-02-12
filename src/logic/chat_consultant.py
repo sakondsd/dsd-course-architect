06c8c3d354023c53e0f572b005003b62a6564cfc
artist_auto <artist_auto@hotmail.com>
2026-02-12 14:13:33 +0700

    Edit AIt

--- src/logic/chat_consultant.py
+++ src/logic/chat_consultant.py
@@ -12,7 +12,7 @@ def get_chat_llm():
         api_key = st.secrets["GOOGLE_API_KEY"]
     
     return ChatGoogleGenerativeAI(
-        model="gemini-2.0-flash", 
+        model="gemini-2.5-flash", 
         google_api_key=api_key,
         temperature=0.2
     )
